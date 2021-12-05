cb26eed8ca98da94b6bfe760a44b92a3f88187ab
amalm9479 <amalm9479@gmail.com>
2021-12-05 06:57:16 +0300

    first

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: node test-api.js
---
+++ test-api.js
@@ -0,0 +1,46 @@
+//jshint esversion:6
+const express = require("express");
+const https = require("https");
+const bodyParser = require("body-parser");
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/",function (req,res){
+  res.sendFile(__dirname + "/index.html");
+});
+
+
+app.post("/",function(req,res){
+  const city =req.body.cityName;
+  const url ="https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=653a1d51c7cb051a6c17cd0d29593c21&units=metric";
+  https.get(url,function(response){
+    console.log(response.statusCode);
+
+    response.on('data',function(data){
+      const weatherDate = JSON.parse(data);
+      const temp = weatherDate.main.temp;
+      const description = weatherDate.weather[0].description;
+      const icon = weatherDate.weather[0].icon;
+      const imageurl = "http://openweathermap.org/img/wn/"+icon+"@2x.png";
+      console.log(temp);
+      console.log(description);
+
+      res.write("<h1>the temperature in "+city+" is "+temp +" degree celcius</h1>"+"<li>the weather is like "+description+"</li>");
+      res.write("<img src="+imageurl+">");
+      res.send();
+    });
+
+
+
+  });
+
+});
+
+
+
+
+  app.listen(process.env.PORT || 3000 ,function(){
+    console.log("its work");
+
+});
